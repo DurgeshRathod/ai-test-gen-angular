5b6b4a2412a14d0ad5d80dc56b93144e318454e5
DurgeshRathod <durgeshrathod.777@gmail.com>
2023-09-21 15:12:26 +0530

    generate unit tests

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # ai-test-gen-angular
+
 **ai-test-gen-angular** is a powerful tool that leverages OpenAI's advanced capabilities to automate the generation of unit tests for your Angular components and services. This innovative package intelligently analyzes your codebase and produces comprehensive test cases, saving you time and effort in the testing process. Seamlessly integrate it into your Angular projects and watch as it enhances your development workflow by ensuring robust and reliable code.
 
-Key Features:
+## Key Features:
 
 - Harnesses the power of OpenAI for intelligent test generation.
 - Automates the creation of unit tests for Angular components and services.
@@ -9,3 +10,26 @@ Key Features:
 - Easily customizable to adapt to specific testing requirements.
 - Streamlines your development process, allowing you to focus on building exceptional applications.
 - Discover a new level of efficiency and confidence in your Angular development with ai-test-gen-angular. Start writing tests
+
+## Usage
+
+To get started with `ai-test-gen-angular`, follow these simple steps:
+
+1. Install the package via npm:
+
+```bash
+npm i ai-test-gen-angular
+```
+
+2. Set Open API key in env
+
+```bash
+export OPEN_API_KEY=somekey
+```
+
+3. Generate Unit Tests (run this command in the root folder of your project)
+
+```bash
+node ./node_modules/ai-test-gen-angular/index.js <src/path/to/component/or/service/ts-file> <src/path/to/ts-config-file>
+
+```
---
+++ index.js
@@ -0,0 +1,153 @@
+const fs = require("fs");
+const OpenAI = require("openai");
+let currPath = process.argv[1];
+currPath = currPath.split("/");
+let projectAbosultePath = currPath.slice(0, currPath.length - 3).join("/");
+
+if (!process.env.OPEN_API_KEY) {
+  throw Error("Environment variable OPEN_API_KEY is missing");
+}
+const openai = new OpenAI({
+  apiKey: process.env.OPEN_API_KEY,
+});
+let prompt_text = "";
+let targetFilePath = process.argv[2];
+targetFilePath = targetFilePath
+  .split("/")
+  .filter((f) => f !== "." || f !== "..")
+  .join("/");
+targetFilePath = `${projectAbosultePath}/${targetFilePath}`;
+if (process.argv.length !== 4) {
+  console.log(
+    "\nERROR: Incorrect command. \n\nUSAGE : node ./node_modules/ai-test-gen-angular/index.js <src/path/to/component/or/service/ts-file> <src/path/to/ts-config-file>\n"
+  );
+  return;
+}
+let tsconfigPath = `${projectAbosultePath}/tsconfig.json`;
+if (process.argv[3]) {
+  tsconfigPath = `${projectAbosultePath}/${process.argv[3]}`;
+} else {
+  throw Error(
+    "Incorrect command. Usage : node ./node_modules/ai-test-gen-angular/index.js <src/path/to/component/or/service/ts-file> <src/path/to/ts-config-file>"
+  );
+}
+let tsconfig = { compilerOptions: { path: {} } };
+try {
+  console.log("Reading ", tsconfigPath);
+  tsconfig = require(tsconfigPath);
+} catch (error) {
+  console.error("ERROR: tsconfig not present at root folder\n", error);
+  return;
+}
+
+let pathMap = {};
+if (tsconfig.compilerOptions && tsconfig.compilerOptions.paths) {
+  let pathObj = tsconfig.compilerOptions.paths;
+  for (let key in pathObj) {
+    if (Object.hasOwnProperty.call(pathObj, key)) {
+      let value = pathObj[key];
+      let key1 = key.replace(/\*/g, "");
+      value = value[0].replace(/\*/g, "");
+      pathMap[key1] = value;
+    }
+  }
+}
+
+let outputFile = targetFilePath.replace(".ts", ".ai-test-gen.spec.ts");
+let concatenatedData = "";
+let paths = [];
+let targetFilePathElements = targetFilePath.split("/");
+fs.readFile(targetFilePath, "utf8", async (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const pattern = /from ['"]([^'"]+)['"]/g;
+
+  let match;
+  while ((match = pattern.exec(data)) !== null) {
+    paths.push(match[1]);
+  }
+
+  Object.keys(pathMap).forEach((key) => {
+    paths.forEach((path, i) => {
+      if (path.includes(key)) {
+        paths[i] = path.replace(key, pathMap[key]);
+      }
+    });
+  });
+  paths = paths.filter(
+    (p) => p.includes(".model") || p.includes(".enum") || p.includes(".interface")
+  );
+  paths.forEach((path, i) => {
+    let count = countOccurrences(path, "..");
+    if (count > 0) {
+      let tillIdx = targetFilePathElements.length - 1 - count;
+      let newpath = targetFilePathElements.slice(0, tillIdx);
+      path = path.split("/").filter((f) => f !== "..");
+      newpath.push(...path);
+      paths[i] = newpath.join("/");
+    } else {
+      paths[i] = `${projectAbosultePath}/${path}`;
+    }
+  });
+  paths = paths.map((p) => p + ".ts");
+  paths.push(targetFilePath);
+
+  for (const filePath of paths) {
+    try {
+      console.log("Reading ", filePath);
+      concatenatedData += " " + fs.readFileSync(filePath, "utf8");
+    } catch (error) {
+      // Handle error
+    }
+  }
+  main();
+});
+
+function countOccurrences(str, substr) {
+  let count = 0;
+  let position = str.indexOf(substr);
+
+  while (position !== -1) {
+    count++;
+    position = str.indexOf(substr, position + 1);
+  }
+
+  return count;
+}
+
+async function main() {
+  system_prompt =
+    "Act as a angular developer use jasmine to write a unit test file for below code with maximum coverage for statements and branches. Prepare mock data by yourself based on the model/interface/class. Write only the unit test code as string output in the output and nothing else. And very important don't add any single line comments and make sure all the opening brackets are closed properly ";
+  prompt_text = "Code: ```import{";
+  prompt_text += concatenatedData;
+  const ASSISTANT = { role: "system", content: system_prompt };
+  console.log("Creating unit tests");
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo-16k",
+    messages: [ASSISTANT, { role: "user", content: prompt_text }],
+  });
+
+  outputText = response.choices[0].message.content.replace(/\n/g, "");
+  console.log("outputText", outputText);
+  if (outputText.endsWith("```")) {
+    outputText = outputText.slice(0, outputText.length - 3);
+  }
+  if (!outputText.startsWith("{") && !outputText.startsWith("import")) {
+    let idx = outputText.indexOf("import");
+    if (idx >= 0) {
+      outputText = outputText.slice(idx);
+    }
+  }
+  outputText = outputText.startsWith("{") ? `import ${outputText}` : outputText;
+  fs.writeFileSync(outputFile, outputText);
+  outputFile = outputFile
+    .split("/")
+    .filter((f) => f !== "." || f !== "..")
+    .join("/");
+  console.log("File written to the path : ", outputFile);
+  console.log(
+    "\n \nFor better result you can store your class models, types, enum, interface in files with extension \n1) .model.ts \n2) .enum.ts \n3) .interface.ts\n4) reading for files which are exported from a index.ts is currently not supported \n\n NOTE: the generated file may have some extra or missing charecters at the start or end of the file which may need to be removed or added manually."
+  );
+}
